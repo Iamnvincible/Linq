453a35929f19f245797c9d0d7d1fe2aa1418aea4
Lin Jie <ljlovemichael@hotmail.com>
2016-02-05 16:28:21 +0800

    anycountsum

---
+++ linqsln/AnyCountSum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnyCountSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> alist = new List<string>();
+            for (int i = 0; i < 100; i++)
+            {
+                alist.Add(i.ToString());
+            }
+            var re=alist.Any();
+            var ree = alist.Any(x => x.Length == 1);
+            var reee = alist.Count(x => x.Length == 1);
+            var reeee = alist.Sum(x =>int.Parse(x));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ linqsln/whereselect/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whereselect
+{
+    public static class MyLinqImplementation
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in GenerateSequence())
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static IEnumerable<string> GenerateSequence()
+        {
+            var i = 0;
+            while (i++ < 100)
+                yield return i.ToString();
+        }
+    }
+}
